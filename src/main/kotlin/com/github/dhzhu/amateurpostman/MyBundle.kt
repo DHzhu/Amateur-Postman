eec5c698535f8d274b6c9e50d7dea3e925c06588
GitHub Action <action@github.com>
2025-11-24 09:26:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/dhzhu/amateurpostman/MyBundle.kt
+++ src/main/kotlin/com/github/dhzhu/amateurpostman/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.dhzhu.amateurpostman
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/dhzhu/amateurpostman/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/dhzhu/amateurpostman/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.dhzhu.amateurpostman.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
